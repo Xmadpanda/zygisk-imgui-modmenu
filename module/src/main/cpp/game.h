094049b7dcec062a6dda7bb11425498c3eb412bf
Xmadpanda <159286513+Xmadpanda@users.noreply.github.com>
2025-04-02 02:03:16 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -3,6 +3,6 @@
 
 // TODO: change this
 #define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "com.fattoy.swordash.android"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
